0216ac25a45718824623126934bf8c2b3ebb19d2
Sean Massa <smassa@groupon.com>
2012-05-12 20:56:54 -0500

    bars moves the player!

--- web/public/javascripts/mind.js
+++ web/public/javascripts/mind.js
@@ -1,3 +1,13 @@
+
+var moveSpeed = 0;
+var nextBar = 2500;
+var bars = [];
+
+var bg;
+var player;
+var stage;
+var currentKey;
+
 function rand(min, max) {
   return parseInt(Math.random() * max + min, 10);
 }
@@ -48,10 +58,6 @@ window.onload = function(){
   runGame();
 };
 
-var bg;
-var player;
-var stage;
-
 function runGame(){
   var canvas;
 
@@ -74,6 +80,10 @@ function runGame(){
   canvas = $('game');
   stage = new Stage(canvas);
 
+  window.addEventListener('keydown', function(e){
+    currentKey = e.keyCode;
+  });
+
   Ticker.addListener(window);
   Ticker.useRAF = true;
   Ticker.setInterval(17);
@@ -84,15 +94,65 @@ function buildInterfaceIfReady()
   if(!bg || !player) return;
 
   player.x = 120;
-  player.y = 200;
+  player.y = 400;
 
   stage.addChild(bg, player);
+  bars.push(addBar(0, 600));
   stage.update();
 }
 
-var moveSpeed = 1.0;
-function tick(){
+function addBar(x, y) {
+  var text = 'DFGI2G23DFG34DG2SFD82F2SF2SFEWF223';
+  var bar = new Text(text, '30px bold Courier New', '#0F0');
+  bar.x = x;
+  bar.y = y;
+  stage.addChild(bar);
+  return bar;
+}
+
+
+function getKey(){
+  if(currentKey == 37) return 'left';
+  if(currentKey == 39) return 'right';
+}
+
+function tick(elapsed) {
+  // board movement
+  for(var i=0; i < bars.length; i++) {
+    var bar = bars[i];
+    bar.y -= 2;
+
+    var playerBottom = player.y + 65;
+    //console.log(playerBottom + ' / ' + player.y + ' / ' + bar.y);
+    var barTop = bar.y - 22;
+
+    var onBar = barTop < playerBottom && barTop > player.y;
+    var overHole = false;
+
+    if(onBar && !overHole)
+      player.y -= (playerBottom - barTop);
+  }
+
+
+  // player movement
+  if(getKey() == 'left') {
+    moveSpeed = -1.0;
+  } else if(getKey() == 'right') {
+    moveSpeed = 1.0;
+  }
+
   player.x += (moveSpeed * 4.5);
+  if (player.x > 548) { player.x = 548; }
+  if (player.x < 0) { player.x = 0; }
+
+
+  // next tick setup
+  nextBar -= elapsed;
+  if(nextBar<=0) {
+    nextBar = 2000;
+    bars.push(addBar(0, 600));
+  }
+
   stage.update();
 }
 
